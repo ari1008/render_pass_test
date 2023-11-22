d3ff9d3bd4977eae30b7cc9463b507da5553f711
Aristide Fumo <aristide.fumo@sopht.com@sopht.com>
2023-11-22 10:25:38 +0100

    solve

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const userRouter = require('user'); // Chemin vers votre fichier de routes
+
+app.use('/users', userRouter); // Montez le routeur sur le chemin '/users'
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
