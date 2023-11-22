94b1eac831a2cd78191970adcc2724ad743d180f
Aristide Fumo <aristide.fumo@sopht.com@sopht.com>
2023-11-22 10:21:13 +0100

    solve

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgres://cloud:g4mHGf6WLwwlhQxR82joA2tY5LBxqvwR@dpg-clesbbrl00ks739tc7u0-a/cloud_v3ec', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
